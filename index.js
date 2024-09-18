e9ab3a5c27fc4b200c7acb8d999f78b5aaf9cf3b
Timona orado <wisem9086@gmail.com>
2024-09-18 12:57:18 +0300

    Method chaining

---
+++ index.js
@@ -0,0 +1,14 @@
+// let username = window.prompt("Enter your name: ");
+
+// username = username.trim();
+// let letter = username.charAt(0);
+// letter = letter.toUpperCase();
+
+// let extraChars = username.slice(1);
+// extraChars = extraChars.toLowerCase
+// username = letter + extraChars;
+// console.log(username);
+//METHOD CHAINING
+let username = window.prompt("Enter your name: ");
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+console.log(username);
